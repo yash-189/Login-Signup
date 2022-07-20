e2b7c946cb3a2630ae6c754a19d4c1d0609ed77a
Yash <yashsaini189@gmail.com>
2022-07-20 23:40:38 +0530

    commit

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -77,7 +77,7 @@ const Signup = () => {
                   type="text"
                   id="name" onChange={onChange} value={register.name} error={errors.name}
                   name="name"
-                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out " placeholder="Name"
+                  className="w-full bg-white rounded border border-gray-300 focus:border-indigo-500 focus:ring-2 focus:ring-indigo-200 text-base outline-none text-gray-700 py-1 px-3 leading-8 transition-colors duration-200 ease-in-out " 
                 />
               </div>
 
